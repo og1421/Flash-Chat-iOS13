c55de778165338dc09b6e669386ace08d114b8a9
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-07-01 18:02:39 -0300

    Update message cell

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,26 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Orlando Moraes Martins on 01/07/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
