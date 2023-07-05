a0c1989609e573ac86e0d946f2b260388bcee16f
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-07-04 21:27:13 -0300

    Update Chat view controller

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -35,7 +35,11 @@ class ChatViewController: UIViewController {
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField : messageSender, K.FStore.bodyField: messageBody], completion: {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField : messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+                ], completion: {
                 (error) in
                 if let e = error {
                     print("There is an issue saving data to firestore, \(e)")
@@ -47,9 +51,12 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages(){
-        messages = []
-        
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+            
             if let e = error {
                 print("There was an issue retrieving data from server ")
             } else {
