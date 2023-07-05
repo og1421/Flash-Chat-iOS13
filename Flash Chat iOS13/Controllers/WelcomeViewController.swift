c52ac495a218ef6eaeefab0375a3ff5ce9e0ec72
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-07-04 22:03:17 -0300

    Update

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,10 +14,12 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         navigationController?.isNavigationBarHidden = false
     }
     
